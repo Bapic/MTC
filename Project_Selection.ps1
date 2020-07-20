69525883800fa75848e7b28c62b4f5fa04bbe846
maanan <41680326+Zuig4turi@users.noreply.github.com>
2020-07-20 12:47:05 +0530

    updating project selection

---
+++ Project_Selection.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host = "Please select the project no"
+(Get-ChildItem -Path .\Root).Name
+$choice = Read-Host ":"
+Write-Host = "Please select the demo to be deployed"
+$Project_name = (Get-ChildItem (".\Root\" + $choice + "-Project*")).name
+Write-Host = "Please select the demo to be deployed"
+(Get-ChildItem (".\Root\" + $Project_name)).Name | ?{$_ -like "*-demo*"}
+$project = Read-Host ":"
+
+$script_path=(Get-ChildItem (".\Root\" + $Project_name +"\" + $project + "-demo*")).FullName
